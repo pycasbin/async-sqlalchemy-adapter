a3237386df6f332cf7494f6c8bf52a1e350ce391
amazingguni <amazingguni@gmail.com>
2023-07-13 18:31:07 +0900

    fix: Use sqlalchemy.orm.declarative_base (#7)
    * sqlalchemy.orm.ext.declarative_base is deprecated(from 1.4)

--- casbin_async_sqlalchemy_adapter/adapter.py
+++ casbin_async_sqlalchemy_adapter/adapter.py
@@ -18,9 +18,8 @@ from casbin import persist
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import or_
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.future import select
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 Base = declarative_base()
 
